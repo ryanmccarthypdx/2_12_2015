d04e12119d17f81cd7e21e6ffb5dea4b4bbd8cb1
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-02-12 10:17:40 -0800

    Add return functuality

--- js/word-ranker-scripts.js
+++ js/word-ranker-scripts.js
@@ -2,7 +2,9 @@ var wordRanker = function(input) {
   var inputSplit = input.split(" ");
   var output = [];
   inputSplit.forEach(function(word){
+    if (output.indexOf(word) === -1) {
     output.push(word);
+    }
   });
 
   return output;
--- spec/word-ranker-specs.js
+++ spec/word-ranker-specs.js
@@ -3,4 +3,15 @@ describe("wordRanker", function() {
     expect(wordRanker("cat")).to.eql(["cat"])
   });
 
+  it("returns multiple words in the order they appeared", function() {
+    expect(wordRanker("dog cat")).to.eql(["dog", "cat"])
+  });
+
+  it("returns returns for returns returns returns", function() {
+    expect(wordRanker("returns returns")).to.eql(["returns"])
+  });
+
+  it("returns multiple words in order of how often they appeared", function() {
+    expect(wordRanker("dog cat cat")).to.eql(["cat", "dog"])
+  });
 });
